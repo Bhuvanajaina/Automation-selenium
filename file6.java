313e5376023072beaf0bf1555f5336fd6d4d986d
Bhuvana <bhuvanajaina@gmail.com>
2024-11-28 18:51:18 +0530

    new file added

---
+++ file6.java
@@ -0,0 +1,17 @@
+public class A{
+
+public void static main(String[] args) {
+
+System.out.print("Hello");
+
+
+System.out.print("Hello2");
+
+System.out.print("Hello3");
+
+System.out.print("Hello4");
+
+System.out.print("Hello5");
+
+}
+}
